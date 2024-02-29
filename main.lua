b61a65d98c74eff9da8dcf0466c3bf68c902687a
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-02-29 20:37:42 +0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+--[[
+THIS FILE WAS OBFUSCATED WITH DDxCN Obfuscator on discord
+]]--
+
+local A = "SetGameSuccessfull";local B = "GMHelper.lua"; (5435=436=436=43=56=34=5==34==454565=554-48) - (4874-435) = (45P45(454587(457834))) else then A; end isGameLoaded(true) else then B = table.remove(".. GMHelper.lua ..") else (45(45(45($5(45))))) end return Main
