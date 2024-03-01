98b02d339a8a268c65805893cb66f9095f836914
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-01 20:33:31 +0500

    Create remove-update-popup.lua

---
+++ remove-update-popup.lua
@@ -0,0 +1,15 @@
+--[[
+-- --> BLOK SSTOOL
+-- --> ANTI HOOK
+-- --> ADDED PASSWORD
+-- --> ADDED VERIFY HUMAN CODE
+-- --> ADDED INFINITY TIME CODE
+
+ ----- 
+-
+-///--
+-////-
+------ameguardian obfuscator (SDR ENC v3)
+]]--
+
+local v0 = {} (3454(3748(4578+4567345(45375[45735[356735-453]])))) elseif 384-457 474=476 34 end; break;if v0 then function(v7, v4)
