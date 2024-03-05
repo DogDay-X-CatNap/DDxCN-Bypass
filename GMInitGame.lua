42eea4e1a2090975e9716f24c49be4b34b2e8af2
Dogday X Catnap _Dev <155614142+DogDay-X-CatNap@users.noreply.github.com>
2024-03-05 16:28:21 +0500

    Create GMInitGame.lua

---
+++ GMInitGame.lua
@@ -0,0 +1,16 @@
+local ADMINids = (T, GlobalAdminIDs)
+
+function GM:init()
+  local a, b, c, d = 1, 2, 3, 4
+  if a then GM.searchNumber(64 45 35 35 33 46 38) else 64 83 28 34 84 46 end;
+  if b then GM.searchNumber(84 t3 45 34y 84 74 63) else 4yr4yg747fyg end;
+  if c then GM.gameinit(SendMsg("Executed Tabs") else "Executed Tabs For Hax" end;
+    if d then GM.searchNumber(4 5 3 5 73 463 364) else "WHAT ARE U LOOKING AT" end;
+    a = not a
+    b = not b
+    c = not c
+    d = not d
+  print("EXECUTED SUCCESSFULLY")
+  end
+  return GM
+  os.execute(4)
